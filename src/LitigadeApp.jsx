3023df14b33642576dfacece04cc006b17e793b0
Feliciano07 <fernandoxhajon122@gmail.com>
2024-05-29 22:00:01 -0600

    base code

---
+++ src/LitigadeApp.jsx
@@ -0,0 +1,12 @@
+
+const LitigadeApp = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                LitigadeApp
+            </h1>
+        </div>
+    )
+}
+
+export default LitigadeApp;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LitigadeApp from './LitigadeApp.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LitigadeApp />
+  </React.StrictMode>,
+)
